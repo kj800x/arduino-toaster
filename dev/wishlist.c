71eed0afea7b803ed9608c22de49494c2219fc4f
Kevin Johnson <kevin@coolkev.com>
2016-02-11 19:20:55 -0500

    Clean up

--- dev/wishlist.c
+++ dev/wishlist.c
@@ -1,15 +1,17 @@
+ALL NAMES ARE DIFFERENT EVERYWHERE
+
 //Top level functions
-loop() -- code to do the main loop.
+[ ] - loop() -- code to do the main loop.
 
 //First level looping functions
-runOven() -- code to run the oven using the shared data stored during the programOven() phase
-programOven() -- collect data for the runOven() phase
+[ ] - runOven() -- code to run the oven using the shared data stored during the programOven() phase
+[ ] - programOven() -- collect data for the runOven() phase
 
 //Helpful functions for runOven
 //Data-in functions
-timeSinceRunStart() -- get the time since the runOven() phase started
-getCurrentExpectedTemperature() -- get the current expected temperature
-readTemperatureFromProbes() -- get the current actual temperature
+[X] - timeSinceRunStart() -- get the time since the runOven() phase started
+[ ] - getCurrentExpectedTemperature() -- get the current expected temperature
+[X] - readTemperatureFromProbes() -- get the current actual temperature
 //Program-out functions
-setFanSpeed() -- update the speed of the fans
-setHeaterOutput() -- update the heater output level
+[X] - setFanSpeed() -- update the speed of the fans
+[X] - setHeaterOutput() -- update the heater output level

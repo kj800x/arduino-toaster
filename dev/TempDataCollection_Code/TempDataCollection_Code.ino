8b9f3eccf0be47ca96caca0558a2762638470402
domyam97 <domyam97@gmail.com>
2016-03-03 17:48:38 -0500

    Heating Tests Continue
    Temp Data Collection now includes a mock profile to test, will use to
    see if we exceed max temp and monitor heating speeds. Data file is just
    numbers for matlab

---
+++ dev/TempDataCollection_Code/TempDataCollection_Code.ino
@@ -0,0 +1,222 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
+#define TEMPERATURE_PRECISION 9
+const int SSRPin =  13;  
+const int LEDPin = 6;
+const int FanPin = 12;
+double time = millis()*1000;
+double avTemp;
+boolean Up;
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device addresses
+DeviceAddress insideThermometer, outsideThermometer;
+
+void setup(void)
+{
+  // start serial port
+  Up = true;
+  avTemp = 0;
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  pinMode(SSRPin, OUTPUT);
+
+  // Start up the library
+  sensors.begin();
+
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: "); 
+  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+  
+  
+  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
+  if (!sensors.getAddress(outsideThermometer, 1)) Serial.println("Unable to find address for Device 1"); 
+
+  
+  Serial.print("Device 0 Address: ");
+  printAddress(insideThermometer);
+  Serial.println();
+
+  Serial.print("Device 1 Address: ");
+  printAddress(outsideThermometer);
+  Serial.println();
+
+  // set the resolution to 9 bit
+  sensors.setResolution(insideThermometer, TEMPERATURE_PRECISION);
+  sensors.setResolution(outsideThermometer, TEMPERATURE_PRECISION);
+
+  Serial.print("Device 0 Resolution: ");
+  Serial.print(sensors.getResolution(insideThermometer), DEC); 
+  Serial.println();
+
+  Serial.print("Device 1 Resolution: ");
+  Serial.print(sensors.getResolution(outsideThermometer), DEC); 
+  Serial.println();
+}
+
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  //Serial.print("Temp C: ");
+  Serial.print(tempC);
+ // Serial.print(" Temp F: ");
+ Serial.print(" ");
+  Serial.print(DallasTemperature::toFahrenheit(tempC));
+}
+//function to define profile
+void checktemp()
+{
+    avTemp = (sensors.getTempC(insideThermometer) + sensors.getTempC(outsideThermometer))/2;
+    if (avTemp < 90)
+    {
+     if(Up)
+    { 
+      digitalWrite(SSRPin, HIGH);
+      delay(1500);
+      digitalWrite(SSRPin, LOW);
+      delay(5);
+    }
+     if(!Up)
+    {
+      digitalWrite(FanPin, HIGH);
+    }
+    }
+   if (avTemp >= 100 && avTemp< 150)
+   {    
+    if(Up)
+    {
+      digitalWrite(SSRPin, HIGH);
+      delay(750);
+      digitalWrite(SSRPin, LOW);
+      delay(500); 
+    }
+     if(!Up)
+    {
+      digitalWrite(FanPin, HIGH);
+    }
+ }
+  if (avTemp >= 150 && avTemp < 220)
+  {
+     if(Up)
+     {
+      digitalWrite(SSRPin, HIGH);
+      delay(1300);
+      digitalWrite(SSRPin, LOW);
+      delay(500);
+     }
+    if(!Up)
+    {
+      digitalWrite(FanPin, HIGH);
+    }
+    
+  }
+  if (avTemp >= 220 && avTemp < 240)
+  {
+    if(Up)
+    {
+      digitalWrite(SSRPin, HIGH);
+      digitalWrite(FanPin, HIGH);
+      delay(500);
+      digitalWrite(SSRPin, LOW);
+      delay(500);
+      if(avTemp == 235)
+      Up = false;
+    }
+    if(!Up)
+    {
+      digitalWrite(FanPin, HIGH);
+    }    
+  }
+   if (avTemp >= 240)
+  {
+    digitalWrite(FanPin, HIGH);
+    digitalWrite(SSRPin, LOW);
+  }
+}
+
+
+void checkTempTest()
+{
+    avTemp = (sensors.getTempC(insideThermometer) + sensors.getTempC(outsideThermometer))/2;
+    if (avTemp < 90)
+    {
+     if(Up)
+    { 
+      digitalWrite(SSRPin, HIGH);
+      delay(1500);
+      digitalWrite(SSRPin, LOW);
+      delay(5);
+      if (avTemp == 90)
+      Up = false;
+    }
+     if(!Up)
+    {
+      digitalWrite(FanPin, HIGH);
+    }
+    }
+}
+// function to print a device's resolution
+void printResolution(DeviceAddress deviceAddress)
+{
+ // Serial.print("Resolution: ");
+ // Serial.print(sensors.getResolution(deviceAddress));
+  //Serial.println();    
+}
+
+// main function to print information about a device
+void printData(DeviceAddress deviceAddress)
+{
+ // Serial.print("Device Address: ");
+ // printAddress(deviceAddress);
+ // Serial.print(" ");
+  printTemperature(deviceAddress);
+  
+}
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    // zero pad the address if necessary
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+void loop(void)
+{ 
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  //Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures();
+  //Serial.println("DONE");
+  
+checktemp();
+
+  // print the device information
+  Serial.print(time);
+  Serial.print(" ");
+  printData(insideThermometer);
+  Serial.print(" ");
+  printData(outsideThermometer);
+  Serial.print(" ");
+  Serial.print(avTemp); 
+  Serial.println();
+}
